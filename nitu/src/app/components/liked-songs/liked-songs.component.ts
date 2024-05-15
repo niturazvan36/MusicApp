f7ed4f70cad258f19ee2767ae8907d3a45aad371
Razvan Nitu <razvan-gabriel.nitu@continental.com>
2024-05-15 14:28:56 +0300

    table

--- nitu/src/app/components/liked-songs/liked-songs.component.ts
+++ nitu/src/app/components/liked-songs/liked-songs.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './liked-songs.component.css'
 })
 export class LikedSongsComponent {
-
+  likedSongs:string[] = ["Hip-Hop","Punk","Rock","Alternative","Indie","Latin","Classical","Jazz","Soul","Blues"];
 }
